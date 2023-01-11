e541aadaa36fd5921e443d28802b5a9c569b55fb
Marcus Silva <mvsilva1997@gmail.com>
2023-01-10 21:19:20 -0300

    variaveis, funções e componente simples

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// IMPORTANDO COMPONENTE
+import FirstComponent from "./components/FirstComponent";
 
 function App() {
+  // VARIAVEIS
+  const name: string = "Átila";
+  const age: number = 25;
+  const isWorking: boolean = false;
+
+  // FUNÇÕES
+
+  /*
+  const userGreeting = (n: string): string => {
+    return `Olá, ${n}`;
+  };
+  */
+
+  function userGreeting(n: string): string {
+    return `Olá, ${n}`;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React com TypeScript</h1>
+
+      {/* VARIAVEIS */}
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <div>
+          <p>Está trabalhando!</p>
+        </div>
+      )}
+
+      {/* FUNÇÕES */}
+      <h3>{userGreeting(name)}</h3>
+
+      {/* COMPONENTE SIMPLES */}
+      <FirstComponent />
     </div>
   );
 }
